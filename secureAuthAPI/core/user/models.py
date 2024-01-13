5f58649d6300d73fdd108c2b17ebf62c96b44c1d
DJDarkCyber <danieljoe.2004@protonmail.ch>
2024-01-14 01:22:06 +0530

    Coded User model

--- secureAuthAPI/core/user/models.py
+++ secureAuthAPI/core/user/models.py
@@ -1,3 +1,36 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.core.exceptions import ObjectDoesNotExist
+from django.db import models
+from django.http import Http404
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    public_id = models.UUIDField(db_index=True, unique=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(db_index=True, max_length=255, unique=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    gender = models.BooleanField()
+
+    email = models.EmailField(db_index=True, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+
+    bio = models.TextField(null=True)
+    avatar = models.ImageField(null=True)
+
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return f"{self.email}"
+    
+    @property
+    def name(self):
+        return f"{self.first_name} {self.last_name}"
