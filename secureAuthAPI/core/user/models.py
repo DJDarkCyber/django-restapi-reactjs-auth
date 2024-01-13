957a1760a152cd15760c25069e2dd9f66bde1be6
DJDarkCyber <danieljoe.2004@protonmail.ch>
2024-01-14 01:34:13 +0530

    Coded User model manager

--- secureAuthAPI/core/user/models.py
+++ secureAuthAPI/core/user/models.py
@@ -6,6 +6,44 @@ from django.db import models
 from django.http import Http404
 
 
+class UserManager(BaseUserManager):
+    def get_object_by_public_id(self, public_id):
+        try:
+            instance = self.get(public_id=public_id)
+            return instance
+        except (ObjectDoesNotExist, ValueError, TypeError):
+            return Http404
+    
+    def create_user(self, username, email, password=None, **kwargs):
+        if username is None:
+            raise TypeError("Users must have a username.")
+        if email is None:
+            raise TypeError("Users must have an email.")
+        if password is None:
+            raise TypeError("Users must have a password.")
+    
+        user = self.model(username=username, email=self.normalize_email(email), **kwargs)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+    
+    def create_superuser(self, username, email, password, **kwargs):
+        if password is None:
+            raise TypeError("Users must have a password.")
+        if email is None:
+            raise TypeError("Users must have an email.")
+        if username is None:
+            raise TypeError("Users must have a username.")
+        
+        user = self.create_user(username, email, password, **kwargs)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
 class User(AbstractBaseUser, PermissionsMixin):
     public_id = models.UUIDField(db_index=True, unique=True, default=uuid.uuid4, editable=False)
     username = models.CharField(db_index=True, max_length=255, unique=True)
