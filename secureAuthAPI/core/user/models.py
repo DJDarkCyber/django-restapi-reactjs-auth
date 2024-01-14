5f942a4eddf6fe235703521d141f174efe57c9a7
DJDarkCyber <danieljoe.2004@protonmail.ch>
2024-01-14 22:28:20 +0530

    Now gender values can be null

--- secureAuthAPI/core/user/models.py
+++ secureAuthAPI/core/user/models.py
@@ -49,7 +49,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(db_index=True, max_length=255, unique=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    gender = models.BooleanField()
+    gender = models.BooleanField(null=True)
 
     email = models.EmailField(db_index=True, unique=True)
     is_active = models.BooleanField(default=True)
