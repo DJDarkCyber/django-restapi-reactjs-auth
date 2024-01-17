e8ee0949d08eb0c251407862c38012d45a5759c8
DJDarkCyber <danieljoe.2004@protonmail.ch>
2024-01-17 20:21:34 +0530

    declared cors

--- secureAuthAPI/secureAuthAPI/settings.py
+++ secureAuthAPI/secureAuthAPI/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "core.auth",
     "rest_framework",
     "rest_framework_simplejwt",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -53,6 +54,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = "secureAuthAPI.urls"
@@ -138,3 +140,5 @@ REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": 
         ["django_filters.rest_framework.DjangoFilterBackend"],
 }
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000"]
